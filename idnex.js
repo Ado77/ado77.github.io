df2c0ea8be10387be9e493d88a40b39a2dc051de
Adam <Ado77@users.noreply.github.com>
2022-07-05 21:56:14 +1000

    Upgrade to lit

---
+++ idnex.js
@@ -0,0 +1,20 @@
+import { LitElement, html } from "https://cdn.skypack.dev/lit-element";
+import { until } from "https://cdn.skypack.dev/lit-html/directives/until.js";
+
+// Working imports
+// import { LitElement, html } from "https://jspm.dev/lit-element";
+// import { until } from "https://jspm.dev/lit-html/directives/until.js";
+
+function example() {
+  return new Promise((a, r) => {
+    setTimeout(() => a("World"), 1000);
+  });
+}
+
+class MyElement extends LitElement {
+  render() {
+    return html`Hello ${until(example(), "Loading ...")}!`;
+  }
+}
+
+customElements.define("my-element", MyElement);
